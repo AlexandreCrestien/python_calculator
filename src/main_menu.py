0d2dc4c1133d3a20d1c08b32865a32b275b552dc
Hazel <hazel.claire.lee@gmail.com>
2025-12-02 12:26:53 +0100

    feat: integrate multiplications

--- src/main_menu.py
+++ src/main_menu.py
@@ -1,6 +1,7 @@
 import os
 from operations.addition import addition
 from operations.subtraction import subtraction
+from operations.multiplication import multiplication
 
 class Menu:
     def __init__(self) -> None:
@@ -36,6 +37,14 @@ class Menu:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 self.calculate()
                 result = subtraction(self.first_number, self.second_number)
+                print(f"{self.first_number} - {self.second_number} = {result}")
+                input("Appuyez sur une touche...")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                self.user_selected_option = False
+            case 3:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                self.calculate()
+                result = multiplication(self.first_number, self.second_number)
                 print(f"{self.first_number} - {self.second_number} = {result}")
                 input("Appuyez sur une touche...")
                 os.system('cls' if os.name == 'nt' else 'clear')
